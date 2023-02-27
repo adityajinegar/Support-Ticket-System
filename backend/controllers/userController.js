5281b4d2d0ac37b1bc9bac3caae288631d0e0a92
Aditya Jinegar <aditya_jinegar@yahoo.com>
2023-02-26 20:49:07 -0500

    protect routes and authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -72,6 +72,18 @@ const loginUser = asyncHandler(async (req, res) => {
   res.send('Login Route');
 });
 
+// @desc   Get current user
+// @route  /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
+});
+
 // Generate token
 const generateToken = (id) => {
   return jwt.sign({ id: id }, process.env.JWT_SECRET, { expiresIn: '30d' });
@@ -80,4 +92,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
