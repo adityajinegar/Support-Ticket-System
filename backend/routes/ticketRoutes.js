39a373dfd79df78404690cab9971cf2f869173cb
Aditya Jinegar <aditya_jinegar@yahoo.com>
2023-03-02 01:13:36 -0500

    single ticket, update and delete

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -2,11 +2,20 @@ const express = require('express');
 const router = express.Router();
 const {
   getTickets,
+  getSingleTicket,
   createTickets,
+  deleteTicket,
+  updateTicket,
 } = require('../controllers/ticketController');
 
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').get(protect, getTickets).post(protect, createTickets);
+
+router
+  .route('/:id')
+  .get(protect, getSingleTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
