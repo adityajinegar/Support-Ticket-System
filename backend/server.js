390518b7991b23a5a056f383ae0294a4495d2cd4
Aditya Jinegar <aditya_jinegar@yahoo.com>
2023-03-07 04:47:12 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,10 @@ app.use('/api/tickets', require('./routes/ticketRoutes'));
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) =>
-    res.sendFile(__dirname, '../frontend', 'build', 'index.html'),
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
   );
 } else {
   app.get('/', (req, res) => {
