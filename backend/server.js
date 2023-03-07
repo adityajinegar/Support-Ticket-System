c75bd4f19929efb3c568cff2fe593e094e0f4f52
Aditya Jinegar <aditya_jinegar@yahoo.com>
2023-03-07 02:56:49 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,8 @@ const PORT = process.env.PORT || 5000;
 // Connect to databse
 connectDB();
 
-app.use(cors());
-
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
