451b5d0c1ae60c77a2a40ba950a65e65c6dda9ea
Aditya Jinegar <aditya_jinegar@yahoo.com>
2023-03-09 01:36:07 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,18 +31,16 @@ app.use('/api/tickets', require('./routes/ticketRoutes'));
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
-  app.use(express.static('frontend/build'));
+  app.use(express.static('frontend/build/'));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
   );
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' });
+  });
 }
-// else
-// {
-//   app.get('/', (req, res) => {
-//     res.status(200).json({ message: 'Welcome to the Support Desk API' });
-//   });
-// }
 
 app.use(errorHandler);
 
