810f5988bfc25cb4af6fd9d3e0b52a3d4d128a5e
Aditya Jinegar <aditya_jinegar@yahoo.com>
2023-03-09 01:08:02 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -12,17 +12,17 @@ connectDB();
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors(),
-  // {
-  // origin: [
-  //   'http://localhost:3000',
-  //   'https://support-ticket-system-static.onrender.com',
-  // ],
-  // }
-);
+app.use(express.json({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(
+//   cors(),
+//   // {
+//   // origin: [
+//   //   'http://localhost:3000',
+//   //   'https://support-ticket-system-static.onrender.com',
+//   // ],
+//   // }
+// );
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
@@ -36,11 +36,13 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
   );
-} else {
-  app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk API' });
-  });
 }
+// else
+// {
+//   app.get('/', (req, res) => {
+//     res.status(200).json({ message: 'Welcome to the Support Desk API' });
+//   });
+// }
 
 app.use(errorHandler);
 
